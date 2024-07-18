34ea74f028e784011df73d4c24160a28580b4eb5
marcolivierbouch <mo.bouchard1997@gmail.com>
2024-07-18 06:49:54 -0400

    feat: run prettier

--- packages/assistant/src/assistant-response.test.ts
+++ packages/assistant/src/assistant-response.test.ts
@@ -1,9 +1,9 @@
-import { expect } from "vitest";
-import { it } from "vitest";
-import { describe } from "vitest";
+import { expect } from 'vitest';
+import { it } from 'vitest';
+import { describe } from 'vitest';
 
 describe('Assistant Resonse', () => {
-    it('should send text', async () => {
-      expect(true);
-    });
+  it('should send text', async () => {
+    expect(true);
+  });
 });
