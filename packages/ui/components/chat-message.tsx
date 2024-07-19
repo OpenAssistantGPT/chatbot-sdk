a5fe21dfd31f8e5d4f5b2336cf5540267dbd38ba
marcolivierbouch <mo.bouchard1997@gmail.com>
2024-07-19 16:21:00 -0400

    feat: update ci

--- packages/ui/components/chat-message.tsx
+++ packages/ui/components/chat-message.tsx
@@ -131,7 +131,7 @@ export function ChatMessage({
                       ) {
                         return (
                           <MathJaxContext>
-                            { /*@ts-ignore*/ }
+                            {/*@ts-ignore*/}
                             <MathJax>{children || ''}</MathJax>
                           </MathJaxContext>
                         );
