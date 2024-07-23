0c4aeb5fde733d07a6de83da4c793c648f5cb204
marcolivierbouch <mo.bouchard1997@gmail.com>
2024-07-23 08:54:15 -0400

    feat: add analytics

--- examples/next-website/app/layout.tsx
+++ examples/next-website/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@openassistantgpt/ui/dist/index.css';
 
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics/> 
       <body className={inter.className}>{children}</body>
     </html>
   );
