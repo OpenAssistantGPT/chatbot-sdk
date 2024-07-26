2ac03b862f55318cb48e43b429e43988115f6492
marcolivierbouch <mo.bouchard1997@gmail.com>
2024-07-25 20:40:31 -0400

    feat: remove button component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,6 @@
 import './global.css';
 import { OpenAssistantGPTChat } from '@/components/chat';
 import { ChatbotConfig } from './chatbot';
-import { OpenAssistantGPTButton } from '@/components/chatbot-button';
 
 export { type ChatbotConfig };
-export { OpenAssistantGPTChat, OpenAssistantGPTButton };
+export { OpenAssistantGPTChat };
