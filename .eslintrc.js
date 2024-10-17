e7844d88138bc169fee5406309d4cc5e84af5042
marcolivierbouch <mo.bouchard1997@gmail.com>
2024-10-17 09:22:21 -0400

    feat: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     },
   },
   rules: {
-    '@next/next/no-html-link-for-pages': ['error', 'website/sdk'],
+    '@next/next/no-html-link-for-pages': ['error', 'website/sdk', 'examples'],
     '@next/next/no-html-link-for-pages': [
       'error',
       '@openassistantgpt/assistant',
